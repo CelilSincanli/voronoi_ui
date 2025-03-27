61e8c1f14f889282a9d8363a63459b179c5666d2
CelilSincanli <celil@adastec.com>
2025-03-27 23:46:39 +0300

    Hello World

---
+++ build/CMakeFiles/voronoi_ui.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/voronoi_ui.dir/include/third_parties/imgui/backends/imgui_impl_glfw.cpp.o"
+  "CMakeFiles/voronoi_ui.dir/include/third_parties/imgui/backends/imgui_impl_glfw.cpp.o.d"
+  "CMakeFiles/voronoi_ui.dir/include/third_parties/imgui/backends/imgui_impl_opengl3.cpp.o"
+  "CMakeFiles/voronoi_ui.dir/include/third_parties/imgui/backends/imgui_impl_opengl3.cpp.o.d"
+  "CMakeFiles/voronoi_ui.dir/include/third_parties/imgui/imgui.cpp.o"
+  "CMakeFiles/voronoi_ui.dir/include/third_parties/imgui/imgui.cpp.o.d"
+  "CMakeFiles/voronoi_ui.dir/include/third_parties/imgui/imgui_draw.cpp.o"
+  "CMakeFiles/voronoi_ui.dir/include/third_parties/imgui/imgui_draw.cpp.o.d"
+  "CMakeFiles/voronoi_ui.dir/include/third_parties/imgui/imgui_tables.cpp.o"
+  "CMakeFiles/voronoi_ui.dir/include/third_parties/imgui/imgui_tables.cpp.o.d"
+  "CMakeFiles/voronoi_ui.dir/include/third_parties/imgui/imgui_widgets.cpp.o"
+  "CMakeFiles/voronoi_ui.dir/include/third_parties/imgui/imgui_widgets.cpp.o.d"
+  "CMakeFiles/voronoi_ui.dir/src/main.cpp.o"
+  "CMakeFiles/voronoi_ui.dir/src/main.cpp.o.d"
+  "voronoi_ui"
+  "voronoi_ui.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/voronoi_ui.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/voronoi_ui.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for voronoi_ui.
+# This may be replaced when dependencies are built.
